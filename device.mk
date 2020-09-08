197c7a7d1ffb2463efebaa2b558e333ae616b43b
PIPIPIG233666 <pig.priv@gmail.com>
2020-09-08 16:24:58 -0400

    violet: power-libperfmgr: Extend hidl changes to aidl
     * This commit removes VR/camera hints, updates the HAL naming, and add
       support for DT2W.
    
     * Import pixel-power-ext from google hardware interfaces
    
       Change-Id: I8e40c6e0b39418619270b5c8dec12ce788f559bf
    
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Co-authored-by: AbhiShek Aggarwal <warabhishek@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Change-Id: Id53c9ce373f80d4f1ab97f61da67aaf70e1563d5

--- device.mk
+++ device.mk
@@ -377,6 +377,8 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/xiaomi
 
 # Telephony
