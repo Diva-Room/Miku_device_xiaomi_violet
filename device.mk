fbcb7e0d6c8c64d4463b175e9cc3cf6fe10e6d3a
Undying-yueyue <orgtech007@gmail.com>
2021-10-21 02:17:06 +0000

    Violet: Build bluetooth@1.0 from Aosp

--- device.mk
+++ device.mk
@@ -130,6 +130,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     libbthost_if \
+    android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
