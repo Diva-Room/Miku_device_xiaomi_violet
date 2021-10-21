32d7da81abd627bddf5fdbd8ea01d4c1051ae24e
Undying-yueyue <orgtech007@gmail.com>
2021-10-21 02:24:01 +0000

    Violet: build DRM Packages

--- device.mk
+++ device.mk
@@ -183,7 +183,11 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.0.vendor \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor \
+    android.hardware.drm@1.3.vendor
 
 # Fingerprint feature
 PRODUCT_COPY_FILES += \
