d142ce7163845e9485a9007cd698beea610ee1e3
Undying-yueyue <orgtech007@gmail.com>
2021-11-24 02:27:24 +0000

    violet: Use proton clang to compile kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,6 +58,8 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/violet
 TARGET_KERNEL_CONFIG := vendor/violet-maru_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
 BOARD_RAMDISK_OFFSET := 0x01000000
 
 # Platform
