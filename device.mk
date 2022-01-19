8471337390a0dfd74ce81903781e0ed370b17d59
Undying-yueyue <orgtech007@gmail.com>
2022-01-19 13:54:10 +0100

    violet: Drop parts for now

--- device.mk
+++ device.mk
@@ -168,10 +168,6 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Display/Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
