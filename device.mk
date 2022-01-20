2d839bd0e847fd322f9686dc50f4874553065d48
Undying-yueyue <orgtech007@gmail.com>
2022-01-20 06:53:57 +0100

    Violet: Build vendor variant Radio config 1.2

--- device.mk
+++ device.mk
@@ -389,7 +389,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
     android.hardware.radio.config@1.0.vendor \
-    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.2.vendor \
     android.hardware.radio@1.5 \
     android.hardware.secure_element@1.1 \
     android.hardware.radio.deprecated@1.0 \
