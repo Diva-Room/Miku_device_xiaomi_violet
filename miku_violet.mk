262b73f4a30be8e58ac3136c2d26aff0d53e6907
AmeChan <79135062+AmeChanRain@users.noreply.github.com>
2023-03-16 21:52:34 +0800

    violet: Drop hard-coded fingerprint
    * If we use hard-coded fingerprint, it will lead to unpredictable consequences (such as bootloops) after upgrades (like QPR2), because the package caches still retains information about the old version of resources, causing some classes unable to be found. Package caches will only be wiped if fingerprint changed.

--- miku_violet.mk
+++ miku_violet.mk
@@ -20,12 +20,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys"
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SP3A.220705.003.A1 8672226 release-keys" \
     PRODUCT_NAME="violet"
-
-PRODUCT_PROPERTY_OVERRIDES += ro.build.fingerprint=google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
