50cd3d4e836218818b8946c67a112da5e2ef792b
AmeChanRain <downloadbot007@gmail.com>
2023-10-05 12:32:21 +0000

    violet: Switch to AIDL Wifi package

--- device.mk
+++ device.mk
@@ -449,7 +449,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
